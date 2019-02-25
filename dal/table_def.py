3e4bc169f4ccf6018edbd54b7095b4a9913bc321
Dakad <3106338+Dakad@users.noreply.github.com>
2019-02-25 15:16:26 +0100

    Add  sql_table_definition for Stop

--- dal/table_def.py
+++ dal/table_def.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Enum, Integer, TIMESTAMP
-
+from sqlalchemy import Column, String, Enum, Integer, TIMESTAMP, ForeignKey, Table
+from sqlalchemy.orm import relationship, backref
 
 import enum
 
@@ -14,6 +14,8 @@ class FeedVersion(Base):
     Arguments:
         Base {[type]} -- [description]
     """
+
+    __tablename__ = "feedversions"
 
     id = Column(String(100), primary_key=True)
     size = Column(Integer)
@@ -52,6 +54,8 @@ class Line(Base):
     route_text_color = Column(String(10))
     mode = Column('type', Enum(LineType))
 
+    stops = relationship("Stop", secondary=line_stops, back_populates="lines")
+
     def __init__(self, **data):
         self.number = data['number']
         self.description = data['description']
@@ -59,3 +63,35 @@ class Line(Base):
         self.terminal = data['terminal']
         self.route_color = data['route_color']
         self.route_text_color = data['route_text_color']
+
+
+"""Association table btwn Lines && Stops
+"""
+line_stops = Table("lines_stops", Base.metadata,
+                   Column('line_id', String, ForeignKey('lines.id')),
+                   Column('stop_id', String, ForeignKey('stops.id'))
+                   )
+
+
+class Stop(Base):
+    """
+    Entity representing a Stop
+
+    Arguments:
+        feed_id {string} -- The feed_id in GTFS
+    """
+
+    __tablename__ = "stops"
+
+    feed_id = Column(String(10), primary_key=True)
+    tech_id = Column(String(10), nullable=True)
+    description_fr = Column(String(100))
+    description_nl = Column(String(100))
+
+    lines = relationship("Line", secondary=line_stops, back_populates="stops")
+
+    def __init__(self, **data):
+        self.feed_id = data['feed_id']
+        self.tech_id = data['tech_id']
+        self.description_fr = data['description_fr']
+        self.description_nl = data['description_nl']
