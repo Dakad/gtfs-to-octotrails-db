8680f61a89a3f5e348effb74c52cbda50bd605d0
Dakad <3106338+Dakad@users.noreply.github.com>
2019-03-02 12:02:27 +0100

    Use log

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config(object):
 
     DEBUG = os.environ.get('DEBUG', True)
 
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT', False)
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT') is not None
 
     LOG_DIR = os.environ.get('LOG_DIR', _DEF_VAL['LOG_DIR'])
     GTFS_DIR = os.environ.get('GTFS_DIR', _DEF_VAL['GTFS_DIR'])
